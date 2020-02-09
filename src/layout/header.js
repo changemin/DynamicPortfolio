4883f4be76715f33932341ee226ff5f343c6265c
Changemin <bkm.change.min@gmail.com>
2020-02-09 19:11:17 +0900

    NavLink로 교체, 스타일 적용

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 function Header() {
     return (
         <Navbar>
-            <StyledLink to="/home">Home</StyledLink>
-            <StyledLink to="/about">About</StyledLink>
-            <StyledLink to="#">Blog</StyledLink>
+            <StyledLink exact to="../" activeStyle={ActiveStyle}>Home</StyledLink>
+            <StyledLink exact to="/about" activeStyle={ActiveStyle}>About</StyledLink>
+            <StyledLink exact to="./" activeStyle={ActiveStyle}>Blog</StyledLink>
         </Navbar>
     )
 }
 
-const StyledLink = styled(Link)`
+const StyledLink = styled(NavLink)`
     color:#BCBCBC;
-    margin-left: 5%;
-    margin-right:5%;
+    margin: 5% 5%;
+
 `
+
+const ActiveStyle = {
+    color:"#ff0000"
+}
 
 const Navbar = styled.div`
     margin-top:5%;
+
 `
 
 export default Header
