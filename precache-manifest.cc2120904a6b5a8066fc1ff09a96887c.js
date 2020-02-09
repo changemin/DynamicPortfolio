6b7d0145ea6e40eabf5cfc609aaa2905461ac717
Changemin <bkm.change.min@gmail.com>
2020-02-09 12:11:27 +0900

    Updates

---
+++ precache-manifest.cc2120904a6b5a8066fc1ff09a96887c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690f81722a4299fd5c5f65c8665deb7a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "20c3ec90641d7f25a818",
+    "url": "/react-portfolio/static/css/main.b94c1eba.chunk.css"
+  },
+  {
+    "revision": "c4ce0c2138620f57b998",
+    "url": "/react-portfolio/static/js/2.1d4a9773.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-portfolio/static/js/2.1d4a9773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20c3ec90641d7f25a818",
+    "url": "/react-portfolio/static/js/main.ac02e810.chunk.js"
+  },
+  {
+    "revision": "069e45ab0620258a507a",
+    "url": "/react-portfolio/static/js/runtime-main.4cba7a13.js"
+  }
+]);
