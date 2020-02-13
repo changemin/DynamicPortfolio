0791d9f3fa78b47ac40455b99aed9b76730bff4c
Changemin <bkm.change.min@gmail.com>
2020-02-14 01:08:45 +0900

    페이지 링크 단순화

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './layout/header';
-import MainPage from './layout/mainPage';
 import Footer from './layout/footer';
 import Project from './pages/Project';
 import About from './pages/About';
@@ -13,20 +12,11 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className = "App">
-          <div className = "container">
-            <Header />
-            <Route exact path="/" render={props => (
-              <React.Fragment>
-                <MainPage />
-              </React.Fragment>
-            )} />
-          </div>
-        </div>
-        <Route path="/about" component={About} />
-        <Route path="/home" component={Home} />
-        <Route path="/blog" component={Blog} />
-        <Route path="/project" component={Project} />
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/blog" component={Blog} />
+        <Route exact path="/project" component={Project} />
         <Footer />
       </Router>
     )
