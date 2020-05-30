c163d79d54ed607eb15db400eb959f02b3ff3bc9
ByunKyungMin <bkm.change.min@gmail.com>
2020-05-30 14:22:32 +0900

    Implement 'About page mouse position animation

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useSpring, animated } from 'react-spring'
 import AvatarImg from '../content/Avatar.png'
 
+const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
+const trans1 = (x, y) => `translate3d(${x / 20}px,${y / 20}px,0)`
+const trans2 = (x, y) => `translate3d(${x / 25 + 15}px,${y / 25 - 0}px,0)`
+const trans3 = (x, y) => `translate3d(${x / 20 - 10}px,${y / 20 - 0}px,0)`
+
 export default function About() {
+    const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
     return (
+        // <div>
+        //     <Name>변경민</Name>
+        //     <Avatar src={AvatarImg} />
+        // </div>
         <div>
-            <Name>변경민</Name>
-            <Avatar src={AvatarImg}/>
+            <div class="container" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+                <div className="Left">
+                    <animated.div className="image1" style={{ transform: props.xy.interpolate(trans1) }} >
+                        <Avatar src={AvatarImg} />
+                    </animated.div>
+                    <animated.div className=" image2" style={{ transform: props.xy.interpolate(trans2) }}>
+                        <Avatar src={AvatarImg} />
+                    </animated.div>
+                    <animated.div className="image3" style={{ transform: props.xy.interpolate(trans3) }}>
+                        <Avatar src={AvatarImg} />
+                    </animated.div>
+                </div>
+            </div>
+
         </div>
     )
 }
