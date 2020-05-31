bf19ddb602a381968306a10e6d1e62f3bb53c48b
ByunKyungMin <bkm.change.min@gmail.com>
2020-05-31 21:29:09 +0900

    Add 'markdown syntax-highlighting'

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
+
+const input = '# This is a header\n\nAnd this is a paragraph \n* hello'
 
 export default function Blog() {
     return (
         <React.Fragment>
-            <h1>이 자리에 블로그가 들어갈거에요</h1>
-            <p>아직 열심히 만들고 있답니당</p>
+            <ReactMarkdown source={input} />
         </React.Fragment>
     )
 }
