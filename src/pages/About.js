5dc3e1879be366e083d38f5f8cef873181f39fa6
ByunKyungMin <bkm.change.min@gmail.com>
2020-05-31 21:05:42 +0900

    Update npm

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,68 +4,40 @@ import { useSpring, animated } from 'react-spring'
 import AvatarImg from '../content/Avatar.png'
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
-const trans1 = (x, y) => `translate3d(${x / 20}px,${y / 20}px,0)`
-const trans2 = (x, y) => `translate3d(${x / 25 + 15}px,${y / 25 - 0}px,0)`
-const trans3 = (x, y) => `translate3d(${x / 20 - 10}px,${y / 20 - 0}px,0)`
+const trans1 = (x, y) => `translate3d(${x / 20}px, ${y / 20}px, 0)`
+const trans2 = (x, y) => `translate3d(${x / 25 + 15}px, ${y / 25 - 0}px, 0)`
+const trans3 = (x, y) => `translate3d(${x / 20 - 10}px, ${y / 20 - 0}px, 0)`
 
 export default function About() {
-    const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
+    const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 600, friction: 140 } }))
     return (
-        // <div>
-        //     <Name>변경민</Name>
-        //     <Avatar src={AvatarImg} />
-        // </div>
-        <div>
-            <div class="container" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
-                <div className="Left">
-                    <animated.div className="image1" style={{ transform: props.xy.interpolate(trans1) }} >
-                        <Avatar src={AvatarImg} />
-                    </animated.div>
-                    <animated.div className=" image2" style={{ transform: props.xy.interpolate(trans2) }}>
-                        <Avatar src={AvatarImg} />
-                    </animated.div>
-                    <animated.div className="image3" style={{ transform: props.xy.interpolate(trans3) }}>
-                        <Avatar src={AvatarImg} />
-                    </animated.div>
-                </div>
-            </div>
-
-        </div>
+        <AboutWrapper onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+            <animated.div style={{ transform: props.xy.interpolate(trans1) }} >
+                
+            </animated.div>
+            <animated.div style={{transform: props.xy.interpolate(trans2) }}>
+                <Avatar src={AvatarImg} />
+            </animated.div>
+            <animated.div style={{ transform: props.xy.interpolate(trans3) }}>
+                <Name>변경민</Name>
+            </animated.div>
+        </AboutWrapper>
     )
 }
 
-const Avatar = styled.img`
-    width: 100px;
+const AboutWrapper = styled.div`
 `
-const Award = styled.li`
-    color: #222;
-    font-family: 'Spoca Han Sans';
-    color: #909090;
-    font-size: 13px;
+
+const Avatar = styled.img`
+    width: 200px;
+    -webkit-filter: drop-shadow(5px 5px 8px #BCBCBC);
+    filter: drop-shadow(5px 5px 8px #BCBCBC);
 `
 
 const Name = styled.h1`
-    color:#222;
-    text-shadow: 5px 5px 8px #BCBCBC; 
-`
-
-const NickName = styled.p`
-    color:#000;
-    font-family:  'Gaegu', cursive;
-    font-weight: 700;
-    word-spacing: -2px;
-`
-const StackContainer = styled.fieldset`
-    float: left;
-    width: 45%;
-    height: inherit;
-    border: 0px;
-    /* border: 2px dotted #ABABAB; */
-    border-radius: 20px;
-`
-
-const AwardContainer = styled(StackContainer)`
-    float: right;
+    text-align: center;
+    position: absolute;
+    left: 50%;
 `
 
 const hashtag = ["03년생", "웹개발자", "디미고"];
